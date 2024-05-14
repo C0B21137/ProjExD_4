5b1ce87c38bc81e99b695e0bd54c0f2c554a533b
c0b21137 <c0b211376a@edu.teu.ac.jp>
2024-05-14 14:59:43 +0900

    feature2

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -228,6 +228,31 @@ class Enemy(pg.sprite.Sprite):
         self.rect.centery += self.vy
 
 
+# class Gravity(pg.sprite.Sprite):
+#     """
+#     画面全体に生成される重力場に関するクラス
+#     """
+#     def __init__(self, life):
+#         """
+#         画面全体に重力場を生成する
+#         引数 life：発動時間
+#         """  
+#         super().__init__()
+#         self.image = pg.Surface((WIDTH, HEIGHT))
+#         pg.draw.rect(self.image, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
+#         self.image.set_alpha(200)
+#         self.rect = self.image.get_rect()
+#         self.life = life
+
+#     def update(self):
+#         """
+#         発動時間を1減算し，発動時間中は重力場を有効にする
+#         """
+#         self.life -= 1
+#         if self.life < 0:
+#             self.kill()
+
+
 class Score:
     """
     打ち落とした爆弾，敵機の数をスコアとして表示するクラス
@@ -258,6 +283,7 @@ def main():
     beams = pg.sprite.Group()
     exps = pg.sprite.Group()
     emys = pg.sprite.Group()
+    #gras=pg.sprite.Group()
 
     tmr = 0
     clock = pg.time.Clock()
@@ -268,6 +294,10 @@ def main():
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
+            # if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
+            #     if score.value > 200:
+            #         gras.add(Gravity(400))
+            #         score.value -= 200
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
@@ -286,6 +316,14 @@ def main():
         for bomb in pg.sprite.groupcollide(bombs, beams, True, True).keys():
             exps.add(Explosion(bomb, 50))  # 爆発エフェクト
             score.value += 1  # 1点アップ
+        
+        # for bomb in pg.sprite.groupcollide(bombs, gras, True, False):
+        #     exps.add(Explosion(bomb, 50))  # 爆発エフェクト
+        #     score.value += 1  # 1点アップ
+        # for emy in pg.sprite.groupcollide(emys, gras, True, False):
+        #     exps.add(Explosion(emy, 100))  # 爆発エフェクト
+        #     score.value += 1  # 1点アップ
+        #     bird.change_img(6, screen)  # こうかとん喜びエフェクト
 
         if len(pg.sprite.spritecollide(bird, bombs, True)) != 0:
             bird.change_img(8, screen) # こうかとん悲しみエフェクト
@@ -294,6 +332,8 @@ def main():
             time.sleep(2)
             return
 
+        # gras.update()
+        # gras.draw(screen)
         bird.update(key_lst, screen)
         beams.update()
         beams.draw(screen)
